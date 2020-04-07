b56f1de8fe8e8be35ccc91d7182b1b877bd4315e
Javier Torres <javier.torres@teseo.es>
2020-04-08 00:35:09 +0200

    deep refactoring in chart update process

--- client/charts.controller.js
+++ client/charts.controller.js
@@ -1,37 +1,69 @@
 angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $http) {
-  
-    $scope.loading=true;
-    $scope.countryCode1 = 'ESP';
-    $scope.countryCode2 = 'ITA';
-    $scope.dayCount = 30;
+    let allData;
 
-    $scope.chartColors = ['#4ea2cc', '#f7464a', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'];
+    $scope.newCasesChart;
 
-    
-    
-    $scope.updateChart = function () {
-        $scope.loading=true;
-        
-        $scope.countryData1 = R.filter(row => row.countryterritoryCode == ($scope.countryCode1), $scope.data).slice(0, $scope.dayCount).reverse();
-        var cases1 = R.map(row => row.cases, $scope.countryData1);
-        var deaths1 = R.map(row => row.deaths, $scope.countryData1);
+    $scope.loading = true;
 
-        $scope.countryData2 = R.filter(row => row.countryterritoryCode == ($scope.countryCode2), $scope.data).slice(0, $scope.dayCount).reverse();
-        var cases2 = R.map(row => row.cases, $scope.countryData2);
-        var deaths2 = R.map(row => row.deaths, $scope.countryData2);
-
-        $scope.chartdata = [cases1, deaths1,cases2,deaths2];
-        $scope.labels = R.map(row => row.day, $scope.countryData1);
-
-        $scope.series = [
-            $scope.countryCode1 + ' cases ', 
-            $scope.countryCode1 + ' deaths ',
-            $scope.countryCode2 + ' cases ',
-            $scope.countryCode2 + ' deaths '
-        ];
-
-        $scope.loading=false;
+    $scope.filter = {
+        countryCode1 : 'ESP',
+        countryCode2 : 'ITA',
+        dayCount : 30
     };
+
+    $scope.chartColors = ['#4ea2cc', '#f7464a', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'];  
+
+    const filterByCountry = function(countryCode, dayCount, data){
+        return R.filter(row => row.countryterritoryCode == (countryCode), data).slice(0, dayCount).reverse();
+    };
+
+    const newCasesChartSpec = {
+        name: 'newCases',
+        functor: function(value){
+            return value; // just raw value
+        },
+        legend: {
+            cases: 'cases',
+            deaths: 'deaths'
+        }
+    };
+
+    const getChartValues = function (chartSpec, filter, countryData1, countryData2) {
+        
+        let cases1 = R.map(row => chartSpec.functor(row.cases), countryData1);
+        let deaths1 = R.map(row => chartSpec.functor(row.deaths), countryData1);
+        let cases2 = R.map(row => chartSpec.functor(row.cases), countryData2);
+        let deaths2 = R.map(row => chartSpec.functor(row.deaths), countryData2);
+
+        let days = R.map(row => row.day, countryData1);
+
+        let chartValues = {
+            chartdata : [cases1, deaths1,cases2,deaths2],
+            labels: days,
+            series: [
+                filter.countryCode1 + chartSpec.legend.cases, 
+                filter.countryCode1 + chartSpec.legend.deaths,
+                filter.countryCode2 + chartSpec.legend.cases,
+                filter.countryCode2 + chartSpec.legend.deaths
+            ]
+        };
+
+        return chartValues;
+
+    };
+
+    $scope.updateAllCharts = function() {
+
+        $scope.loading = true;
+
+        const countryData1 = filterByCountry($scope.filter.countryCode1, $scope.filter.dayCount, allData);
+        const countryData2 = filterByCountry($scope.filter.countryCode2, $scope.filter.dayCount, allData);
+
+        $scope.newCasesChartValues = getChartValues(newCasesChartSpec, $scope.filter, countryData1, countryData2);
+
+        $scope.loading = false;
+        
+    }
 
     
     $http({
@@ -47,11 +79,11 @@ angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $
         method: 'GET',
         url: 'today-data',
         responseType: 'arraybuffer'
-    }).then(function (data) {
-        var wb = XLSX.read(data.data, { type: "array" });
-        $scope.data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
-
-        $scope.updateChart();
+    }).then(function (response) {
+        var wb = XLSX.read(response.data, { type: "array" });
+        allData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+        console.log(allData);
+        $scope.updateAllCharts();
 
     }, function (err) {
         console.log(err);
