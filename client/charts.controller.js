2925bee0b8a95d2edf612f08812c06ef8c79b23a
Javier Torres <javier.torres@teseo.es>
2020-04-08 02:12:47 +0200

    deep refactor v2 and new chart new cases by population

--- client/charts.controller.js
+++ client/charts.controller.js
@@ -1,9 +1,10 @@
 angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $http) {
-    let allData;
+    $scope.allData = [];
 
     $scope.newCasesChart;
 
     $scope.loading = true;
+
 
     $scope.filter = {
         countryCode1 : 'ESP',
@@ -19,8 +20,8 @@ angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $
 
     const newCasesChartSpec = {
         name: 'newCases',
-        functor: function(value){
-            return value; // just raw value
+        functor: function(rowField, row, filter){
+            return row[rowField]; // just raw value
         },
         legend: {
             cases: 'cases',
@@ -28,12 +29,23 @@ angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $
         }
     };
 
+    const newCasesOverPopulationChartSpec = {
+        name: 'newCasesOverPopulation',
+        functor: function(rowField, row, filter){
+            return row[rowField]/row.popData2018 * 100;
+        },
+        legend: {
+            cases: ' cases (%) ',
+            deaths: ' deaths (%)'
+        }
+    };
+
     const getChartValues = function (chartSpec, filter, countryData1, countryData2) {
         
-        let cases1 = R.map(row => chartSpec.functor(row.cases), countryData1);
-        let deaths1 = R.map(row => chartSpec.functor(row.deaths), countryData1);
-        let cases2 = R.map(row => chartSpec.functor(row.cases), countryData2);
-        let deaths2 = R.map(row => chartSpec.functor(row.deaths), countryData2);
+        let cases1 = R.map(row => chartSpec.functor('cases', row, filter), countryData1);
+        let deaths1 = R.map(row => chartSpec.functor('deaths', row, filter), countryData1);
+        let cases2 = R.map(row => chartSpec.functor('cases',row, filter), countryData2);
+        let deaths2 = R.map(row => chartSpec.functor('deaths',row, filter), countryData2);
 
         let days = R.map(row => row.day, countryData1);
 
@@ -52,42 +64,46 @@ angular.module("app", ['chart.js']).controller("ChartsCtrl", function ($scope, $
 
     };
 
-    $scope.updateAllCharts = function() {
+    $scope.updateCharts = function() {
 
         $scope.loading = true;
-
-        const countryData1 = filterByCountry($scope.filter.countryCode1, $scope.filter.dayCount, allData);
-        const countryData2 = filterByCountry($scope.filter.countryCode2, $scope.filter.dayCount, allData);
+        let countryData1 = filterByCountry($scope.filter.countryCode1, $scope.filter.dayCount, $scope.allData);
+        let countryData2 = filterByCountry($scope.filter.countryCode2, $scope.filter.dayCount, $scope.allData);
 
         $scope.newCasesChartValues = getChartValues(newCasesChartSpec, $scope.filter, countryData1, countryData2);
+        $scope.newCasesOverPopulationChartValues = getChartValues(newCasesOverPopulationChartSpec, $scope.filter, countryData1, countryData2);
 
         $scope.loading = false;
         
-    }
+    };
+
+    angular.element(document).ready(function () {
+        $http({
+            method: 'GET',
+            url: '/country-population.json',
+        }).then(function (data) {
+            $scope.countryData = data.data.countryData;
+        }, function (err) {
+            console.log(err);
+        });
+    
+        $http({
+            method: 'GET',
+            url: 'today-data',
+            responseType: 'arraybuffer'
+        }).then(function (response) {
+            var wb = XLSX.read(response.data, { type: "array" });
+            $scope.allData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+    
+            $scope.updateCharts();
+    
+        }, function (err) {
+            console.log(err);
+        });
+    });
 
     
-    $http({
-        method: 'GET',
-        url: '/country-population.json',
-    }).then(function (data) {
-        $scope.countryData = data.data.countryData;
-    }, function (err) {
-        console.log(err);
-    });
-
-    $http({
-        method: 'GET',
-        url: 'today-data',
-        responseType: 'arraybuffer'
-    }).then(function (response) {
-        var wb = XLSX.read(response.data, { type: "array" });
-        allData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
-        console.log(allData);
-        $scope.updateAllCharts();
-
-    }, function (err) {
-        console.log(err);
-    });
+    
 
     
 
